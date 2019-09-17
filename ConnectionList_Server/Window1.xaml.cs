571d41bdba7e97a5e0c2f5dfc9a5daa62c2839ea
mingky <admin@mingky.me>
2019-09-17 11:10:56 +0900

    TCP PING

--- ConnectionList_Server/Window1.xaml.cs
+++ ConnectionList_Server/Window1.xaml.cs
@@ -22,21 +22,19 @@ namespace ConnectionList_Server
         readonly static int unityPort = 6568;
         readonly static int wpfPort = 6569;
         UdpClient udp = new UdpClient(wpfPort);
-        // TcpClient tcp = new TcpClient(new IPEndPoint(IPAddress.Parse("127.0.0.1"),wpfPort));
 
-        IPEndPoint unityEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.27"), unityPort);
-        IPEndPoint anyEndPoint = new IPEndPoint(IPAddress.Any, wpfPort);
+        
         public Window1()
         {
             InitializeComponent();
             
 
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string msg = "DataFromController" + Environment.NewLine + "";
             byte[] msgByteArray = Encoding.UTF8.GetBytes(msg);
+            IPEndPoint unityEndPoint = new IPEndPoint(IPAddress.Parse(XAML_IP.Text), unityPort);
             udp.Send(msgByteArray, msgByteArray.Length, unityEndPoint);
             MessageBox.Show("보냄");
 
